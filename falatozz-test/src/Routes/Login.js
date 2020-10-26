91763d0892616b211c1d2180589160ffe4a2b875
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-26 20:32:12 +0100

    Create basic login page

--- falatozz-test/src/Routes/Login.js
+++ falatozz-test/src/Routes/Login.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./Login.css";
 
 export default function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const history = useHistory();
+
+    function handleChange(e) {
+        if (e.target.name === "email") {
+            setEmail(e.target.value);
+        }
+        if (e.target.name === "password"){
+            setPassword(e.target.value);
+        }
+    };
+
+    function handleLogin(){
+        history.push("/products");
+    }
+
     return(
-        <h1>Login</h1>
+        <div className="login-container">
+            <input name="email" placeholder="Email address" className="input-field" value={email} onChange={handleChange}></input>
+            <input name="password" placeholder="Password" className="input-field" value={password} onChange={handleChange} type="password"></input>
+            <button onClick={handleLogin}>Login</button>
+            <button>Register</button>
+        </div>
     )
 }
