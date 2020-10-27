9ac425e0cd9e705d4d89471039b8af40e9ab64d0
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-27 18:01:44 +0100

    Create Cartitem component for cart page

---
+++ falatozz-test/src/Components/CartItem.js
@@ -0,0 +1,15 @@
+import React, {Row} from "react";
+import "./CartItem.css";
+
+export default function CartItem(props){
+    return(
+        <div className="cartitem-container">
+            <div className="row">
+                <div className="col underline">{props.name}</div>
+                <div className="col underline">{props.price} Ft</div>
+                <div className="col underline">{props.quantity}</div>
+                <div className="col underline">{props.total} Ft</div>
+            </div>
+        </div>
+    )
+}
