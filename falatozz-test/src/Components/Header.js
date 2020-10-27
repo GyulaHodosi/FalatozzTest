58f965daaf451a25e9d3635f6fa180b103bf0ec0
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-27 17:59:35 +0100

    Introduced color variables

--- falatozz-test/src/Components/Header.js
+++ falatozz-test/src/Components/Header.js
@@ -11,11 +11,12 @@ export default function Header(){
     
     return(
         <div className="header">
-            <div className="header-title">Lampi-On</div>
-            <div name="cart" className="header-option" onClick={handleSelectOption}>Cart</div>
-            <div name="products" className="header-option" onClick={handleSelectOption}>Products</div>
-            <div name="register" className="header-option" onClick={handleSelectOption}>Register</div>
-            <div name="login" className="header-option" onClick={handleSelectOption}>Login</div>
+            <img alt="logo" className="logo" src="https://png2.cleanpng.com/sh/268b45b48074a5bd6378895ff6193905/L0KzQYm3VsA6N6t7e5H0aYP2gLBuTfxidaEyhNtwaISwdb3sgCRzcZRujOs2b3LkfbK0jP9od153h9H2LXTyfbP5gb02amJnfNhtZHS1c4HtUb46OGIATqgCN0G4Qom8VcM1O2g9TqU8LoDxd1==/kisspng-lamp-light-electricity-obama-logo-room-dombra-5b1bdfddd2c0f1.9019667715285534378633.png"></img>
+            <div className="header-title">lampify.</div>
+            <div name="cart" className="header-option" onClick={handleSelectOption}>Kosár</div>
+            <div name="products" className="header-option" onClick={handleSelectOption}>Termékek</div>
+            <div name="register" className="header-option" onClick={handleSelectOption}>Regisztráció</div>
+            <div name="login" className="header-option" onClick={handleSelectOption}>Bejelntkezés</div>
         </div>
     )
 }
