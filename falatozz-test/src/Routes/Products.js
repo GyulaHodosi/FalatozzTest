01bd5db9ce825e23880d99d28f6c84f99d2881d1
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-28 11:44:55 +0100

    Create Product data model

--- falatozz-test/src/Routes/Products.js
+++ falatozz-test/src/Routes/Products.js
@@ -1,52 +1,22 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import Product from "../Components/Product";
+import ProductDetail from "../Components/ProductDetail";
 import "./Products.css";
+import { products } from "../Util/ProductDB.js";
+import { addToCart, cart } from "../Util/CartData";
 
 export default function Products() {
     Modal.setAppElement("body");
     const [selected, setSelected] = useState(null);
-    const [quantity, setQuantity] = useState(0);
+    const [quantity, setQuantity] = useState(1);
     const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    const products = [
-        {  
-            id: 1,
-            image: "https://images-na.ssl-images-amazon.com/images/I/51vT8t%2B8YqL._AC_SX466_.jpg",
-            name: "HAITRAL Asztali Lámpa",
-            price: 5600,
-        },
-        {  
-            id: 2,
-            image: "https://ii1.pepperfry.com/media/catalog/product/s/t/1100x1210/stripped-shade-wooden-table-lamp-by-the-light-house-stripped-shade-wooden-table-lamp-by-the-light-ho-k1jtnv.jpg",
-            name: "HAITRAL Asztali Lámpa 2",
-            price: 6500,
-        },
-        {  
-            id: 3,
-            image: "https://images-na.ssl-images-amazon.com/images/I/51vT8t%2B8YqL._AC_SX466_.jpg",
-            name: "HAITRAL Asztali Lámpa",
-            price: 5600,
-        },
-        {  
-            id: 4,
-            image: "https://images-na.ssl-images-amazon.com/images/I/51vT8t%2B8YqL._AC_SX466_.jpg",
-            name: "HAITRAL Asztali Lámpa",
-            price: 5600,
-        },
-        {  
-            id: 5,
-            image: "https://images-na.ssl-images-amazon.com/images/I/51vT8t%2B8YqL._AC_SX466_.jpg",
-            name: "HAITRAL Asztali Lámpa",
-            price: 5600,
-        },
-    ]
-
+        
     function handleSelect(product) {
         setSelected(product)
         openModal();
     }
-
+    
     function openModal() {
         setModalIsOpen(true);
     }
@@ -56,8 +26,21 @@ export default function Products() {
         setQuantity(0);
     }
 
-    function handleChange(e){
-        setQuantity(e.target.value);
+    function handleChangeQuantity(e){
+        if (e.target.getAttribute("name") === "plus") {
+            setQuantity(quantity+1);
+        }
+        if (e.target.getAttribute("name") === "minus") {
+            if(quantity-1 >= 1) {
+                setQuantity(quantity-1);
+            }
+        }
+    }
+
+    function handleAddToCart() {
+        addToCart(selected, quantity);
+        alert(quantity + " elemet a kosárba helyezett!");
+        closeModal();
     }
 
     return(
@@ -71,7 +54,6 @@ export default function Products() {
                 </div>
             </div>
             <Modal
-            styles={{ modal: {}, overlay: { backgroundColor: "#ccc" } }}
             className="modal"
             isOpen={modalIsOpen}
             onRequestClose={closeModal}
@@ -79,16 +61,20 @@ export default function Products() {
             >
             <div className="modal-container">
                 <div className="modal-body">
-                <Product product={selected} handleSelect={handleSelect} />
+                    <ProductDetail product={selected}/>
                 </div>
-                <div>Mennyiség:</div>
-                <input value={quantity} onChange={handleChange}></input>
+                <div className="quantity-title">Mennyiség: </div>
+                <div className="quantitycontrol-container">
+                    <div name="minus" onClick={handleChangeQuantity} className="quantitycontroler">-</div>
+                    <div className="quantity">{quantity}</div>
+                    <div name="plus"onClick={handleChangeQuantity} className="quantitycontroler">+</div>
+                </div>
                 <div className="modal-footer">
-                <button className="modal-button-save" onClick={closeModal}>
+                <button className="modal-button-save" onClick={handleAddToCart}>
                     Kosárba tesz!
                 </button>
                 <button className="modal-button-close" onClick={closeModal}>
-                    Close
+                    Vissza!
                 </button>
                 </div>
             </div>
---
+++ falatozz-test/src/Util/ProductDB.js
@@ -0,0 +1,62 @@
+export const products = [
+    {  
+        id: 1,
+        image: "https://images-na.ssl-images-amazon.com/images/I/51vT8t%2B8YqL._AC_SX466_.jpg",
+        name: "HAITRAL Asztali Lámpa",
+        price: 5600,
+    },
+    {  
+        id: 2,
+        image: "https://ii1.pepperfry.com/media/catalog/product/s/t/1100x1210/stripped-shade-wooden-table-lamp-by-the-light-house-stripped-shade-wooden-table-lamp-by-the-light-ho-k1jtnv.jpg",
+        name: "White Fabric Asztali Lámpa",
+        price: 6500,
+    },
+    {  
+        id: 3,
+        image: "https://ii1.pepperfry.com/media/catalog/product/b/e/568x625/beige-cotton-table-lamp-by-foziq-beige-cotton-table-lamp-by-foziq-ewenfx.jpg",
+        name: "Baige Fabric Asztali Lámpa",
+        price: 12400,
+    },
+    {  
+        id: 4,
+        image: "https://cb2.scene7.com/is/image/CB2/SnakeTableLampSHF19/?$web_zoom$&190430165808&wid=450&hei=450",
+        name: "Snake Asztali Lámpa",
+        price: 8700,
+    },
+    {  
+        id: 5,
+        image: "https://img.vntg.com/large/15687120107881/kosta-boda-glass-table-lamp-by-ove-sandeberg-sweden.jpg",
+        name: "Kosta Boda Asztali Lámpa",
+        price: 17800,
+    },
+    {  
+        id: 6,
+        image: "https://cdn.rockettstgeorge.co.uk/media/catalog/product/cache/dd14b0d3b919ecdf0f91c9c41baf5328/w/h/white-glass-globe-table-lamp-with-black_brass-base.jpg",
+        name: "GassGlobe Asztali Lámpa",
+        price: 7800,
+    },
+    {  
+        id: 7,
+        image: "https://cb2.scene7.com/is/image/CB2/AdaIIWhiteTableLampSHF16/?$web_zoom$&190905021327&wid=450&hei=450",
+        name: "AdaII Asztali Lámpa",
+        price: 10900,
+    },
+    {  
+        id: 8,
+        image: "https://ae01.alicdn.com/kf/H8a1f1c6fb0a0476bb784a0dfe40e61cck/Nordic-Modern-LED-Bedside-Lamp-Bed-Lamp-For-Living-Room-Bedroom-Decorative-Lighting-Desk-Lamp-Table.jpg",
+        name: "Nordic Modern Asztali Lámpa",
+        price: 13900,
+    },
+    {  
+        id: 9,
+        image: "https://xcdn.next.co.uk/Common/Items/Default/Default/ItemImages/AltItemShot/315x472/190840.jpg",
+        name: "Brooklyn Asztali Lámpa",
+        price: 9600,
+    },
+    {  
+        id: 10,
+        image: "https://www.coxandcox.co.uk/media/catalog/product/cache/eceb26e77de98330381661bbe755c5ac/s/s/ss19-h-blshdesk-on_2.png",
+        name: "Blush & Blaze Asztali Lámpa",
+        price: 14400,
+    },
+]
