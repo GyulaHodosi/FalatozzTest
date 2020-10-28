e7874659e24dc5aaa8700e1ad8a639c646ed59c7
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-28 11:42:07 +0100

    add remove button for cart items

--- falatozz-test/src/Components/CartItem.js
+++ falatozz-test/src/Components/CartItem.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { removeFromCart } from "../Util/CartData";
 import "./CartItem.css";
 
 export default function CartItem(props){
+
+    function handleRemove() {
+        removeFromCart(props.id);
+        props.handleChange();
+    }
+
     return(
         <div className="cartitem-container">
             <div className="row">
                 <div className="col underline">{props.name}</div>
                 <div className="col underline">{props.price} Ft</div>
                 <div className="col underline">{props.quantity}</div>
-                <div className="col underline">{props.total} Ft</div>
+                <div className="col underline">{props.total} Ft <div className="delete-cartitem" onClick={handleRemove}>x</div></div>
             </div>
         </div>
     )
