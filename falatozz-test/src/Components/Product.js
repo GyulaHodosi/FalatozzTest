0b7c3710bde4379ea9dae81ffc3b9641aeee3052
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-28 11:43:09 +0100

    add alt to img

--- falatozz-test/src/Components/Product.js
+++ falatozz-test/src/Components/Product.js
@@ -4,7 +4,7 @@ import "./Product.css";
 export default function Product(props) {
     return (
         <div onClick={() => props.handleSelect(props.product)} className="product-container">
-            <img src={props.product.image} className="product-image"></img>
+            <img alt="product" src={props.product.image} className="product-image"></img>
             <div className="product-name">{props.product.name}</div>
             <div className="product-price">{props.product.price} Ft</div>
         </div>
